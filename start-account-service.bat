8ce2fc1635171c2bd8dd836be3cb2cc1e749efce
Allen <you@example.com>
2026-04-25 00:30:27 +0800

    添加运行skywalking的脚本

---
+++ start-account-service.bat
@@ -0,0 +1,9 @@
+@echo off
+chcp 65001 >nul
+
+set SKYWALKING_AGENT_PATH=D:\Develop\skywalking-agent
+set SKYWALKING_COLLECTOR=127.0.0.1:11800
+
+cd /d %~dp0account-service
+
+mvn spring-boot:run -Dspring-boot.run.jvmArguments="-javaagent:%SKYWALKING_AGENT_PATH%\skywalking-agent.jar -Dskywalking.agent.service_name=account-service -Dskywalking.collector.backend_service=%SKYWALKING_COLLECTOR% -Xms512m -Xmx1024m"
---
+++ start-order-service.bat
@@ -0,0 +1,9 @@
+@echo off
+chcp 65001 >nul
+
+set SKYWALKING_AGENT_PATH=D:\Develop\skywalking-agent
+set SKYWALKING_COLLECTOR=127.0.0.1:11800
+
+cd /d %~dp0order-service
+
+mvn spring-boot:run -Dspring-boot.run.jvmArguments="-javaagent:%SKYWALKING_AGENT_PATH%\skywalking-agent.jar -Dskywalking.agent.service_name=order-service -Dskywalking.collector.backend_service=%SKYWALKING_COLLECTOR% -Xms512m -Xmx1024m"
